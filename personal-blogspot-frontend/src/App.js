c69ec01afbdfc7ec7c80be88a490e38a5537d3fd
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-14 21:44:41 +0530

    Added Redirect for unspecified urls

--- personal-blogspot-frontend/src/App.js
+++ personal-blogspot-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import AllBlogs from './users-blogs/container/view-all-blogs';
 import AuthorBlogs from './blogs/container/author-blogs';
@@ -16,6 +16,7 @@ function App() {
           <Route exact path="/blogs/:userId" component={AuthorBlogs}></Route>
           <Route exact path="/blog/new" component={AddBlog}></Route>
           <Route exact path="/blog/:blogId/:userId" component={SingleBlog}></Route>
+          <Redirect to="/"/>
         </Switch>
       </main>
     </Router>
