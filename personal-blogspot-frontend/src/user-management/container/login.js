85d198e7de33e3d55a1882ca2b972ba0e83145aa
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-15 13:52:54 +0530

    Modified login and register into controlled component

--- personal-blogspot-frontend/src/user-management/container/login.js
+++ personal-blogspot-frontend/src/user-management/container/login.js
@@ -3,6 +3,31 @@ import { Link} from 'react-router-dom';
 import Card from '../../shared/components/UIElements/Card';
 
 export default class login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        }
+        this.formChangeHandler = this.formChangeHandler.bind(this);
+        this.formSubmitHandler = this.formSubmitHandler.bind(this);
+    }
+    formChangeHandler(e) {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    formSubmitHandler(e) {
+        e.preventDefault();
+        const loginUser = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        
+        console.log(loginUser);
+        
+    }
     render() {
         return (
             <div className="container">
@@ -10,12 +35,12 @@ export default class login extends Component {
                     <div className="row">
                         <div className="col-md-8 m-auto">
                             <h1 className="display-4 text-center">Log In</h1>
-                            <form >
+                            <form onSubmit={this.formSubmitHandler}>
                                 <div className="form-group">
-                                    <input type="email" required className="form-control form-control-lg"  placeholder="Email Address" name="email" />
+                                    <input type="email" required className="form-control form-control-lg" onChange={this.formChangeHandler} value={this.state.email} placeholder="Email Address" name="email" />
                                 </div>
                                 <div className="form-group">
-                                    <input type="password" required className="form-control form-control-lg"  placeholder="Password" name="password" />
+                                    <input type="password" required className="form-control form-control-lg" onChange={this.formChangeHandler} value={this.state.password}  placeholder="Password" name="password" />
                                 </div>
                                 <input type="submit" value="Login" className="btn btn-info btn-block mt-4" />
                                 <Link to="/register" style={{"textDecoration": "none"}}><button type="button" className="btn btn-outline-info btn-block mt-4">Sign up</button></Link>
--- personal-blogspot-frontend/src/user-management/container/register.js
+++ personal-blogspot-frontend/src/user-management/container/register.js
@@ -2,6 +2,32 @@ import React from "react";
 import { Link} from 'react-router-dom';
 import Card from '../../shared/components/UIElements/Card';
 export default class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name:"",
+            email: "",
+            password: ""
+        }
+        this.formChangeHandler = this.formChangeHandler.bind(this);
+        this.formSubmitHandler = this.formSubmitHandler.bind(this);
+    }
+    formChangeHandler(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    formSubmitHandler(e) {
+        e.preventDefault();
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        console.log(newUser);
+    }
     render() {
         return (
                 <div className="container">
@@ -10,17 +36,17 @@ export default class Register extends React.Component {
                         <div className="col-md-8 m-auto">
                             <h1 className="display-4 text-center">Sign Up</h1>
                             <p className="lead text-center">Create your Account</p>
-                            <form>
+                            <form onSubmit={this.formSubmitHandler}>
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg" placeholder="Name" name="name"
+                                    <input type="text" className="form-control form-control-lg" onChange={this.formChangeHandler} value={this.state.name} placeholder="Name" name="name"
                                         required />
                                 </div>
                                 <div className="form-group">
-                                    <input type="email" className="form-control form-control-lg" required  placeholder="Email Address" name="email" />
+                                    <input type="email" className="form-control form-control-lg" required onChange={this.formChangeHandler} value={this.state.email} placeholder="Email Address" name="email" />
 
                                 </div>
                                 <div className="form-group">
-                                    <input type="password" className="form-control form-control-lg" required placeholder="Password" name="password" />
+                                    <input type="password" className="form-control form-control-lg" required onChange={this.formChangeHandler} value={this.state.password} placeholder="Password" name="password" />
                                 </div>
 
                                 <input type="submit" value="Sign up" className="btn btn-info btn-block mt-4" />
