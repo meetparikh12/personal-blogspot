60b87719a8818b9274ae0838319ba81a8ede25cc
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-16 18:18:39 +0530

    Hash password functionality

--- personal-blogspot-backend/controllers/users.js
+++ personal-blogspot-backend/controllers/users.js
@@ -1,6 +1,8 @@
 const ErrorHandling = require('../models/error-handling');
 const User = require('../models/user');
 const {validationResult}= require('express-validator');
+const bcrypt = require('bcryptjs');
+
 exports.SIGNUP_USER = async (req, res, next) => {
     const error = validationResult(req);
     if(!error.isEmpty()){
@@ -24,11 +26,19 @@ exports.SIGNUP_USER = async (req, res, next) => {
     if (user) {
         return next(new ErrorHandling('Email already exists', 422))
     }
+    let hashedPassword;
+    try {
+         hashedPassword = await bcrypt.hash(password, 12);
+    } catch(err) {
+        return next(new ErrorHandling('Password not hashed', 500));
+    }
+
     user = new User({
         name,
         email,
-        password
+        password: hashedPassword
     })
+    
     try {
         await user.save();
     } catch (err) {
@@ -53,9 +63,18 @@ exports.LOGIN_USER = async (req, res, next) => {
     } catch (err) {
         return next(new ErrorHandling('Invalid Credentials', 422))
     }
-    if (!user || user.password !== password) {
+    if (!user) {
         return next(new ErrorHandling('Invalid Credentials', 422))
     }
+    let result;
+    try {
+         result = await bcrypt.compare(password, user.password)
+    } catch(err) {
+        return next(new ErrorHandling('Password not compared', 422))   
+    }
+    if(!result) {
+        return next(new ErrorHandling('Invalid Credentials', 422))
+    } 
     res.status(200).json({
         user
     })
